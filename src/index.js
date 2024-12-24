b30f6bafba3acd1496309895a536e39795fb5466
yunian132 <67104038+yunian132@users.noreply.github.com>
2024-12-24 19:48:23 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + yunian.site]: dockerHub,
+  ["quay." + yunian.site]: "https://quay.io",
+  ["gcr." + yunian.site]: "https://gcr.io",
+  ["k8s-gcr." + yunian.site]: "https://k8s.gcr.io",
+  ["k8s." + yunian.site]: "https://registry.k8s.io",
+  ["ghcr." + yunian.site]: "https://ghcr.io",
+  ["cloudsmith." + yunian.site]: "https://docker.cloudsmith.io",
+  ["ecr." + yunian.site]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + yunian.site]: dockerHub,
 };
 
 function routeByHosts(host) {
